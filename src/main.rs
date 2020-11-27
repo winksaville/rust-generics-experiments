1f6e71244612c22fde05896b3b137ab383403748
Wink Saville <wink@saville.com>
2020-11-27 14:35:24 -0800

    Add module generics_experiments

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
-fn largest<T: PartialOrd>(list: &[T]) -> &T {
-    let mut largest = &list[0];
+mod generics_experiments {
 
-    for item in list {
-        if item > largest {
-            largest = item;
+    pub fn largest<T: PartialOrd>(list: &[T]) -> &T {
+        let mut largest = &list[0];
+
+        for item in list {
+            if item > largest {
+                largest = item;
+            }
         }
+
+        return largest;
     }
 
-    return largest;
 }
+
+use crate::generics_experiments as ge;
 
 fn main() {
     let number_list = vec![32, 50, 25, 100, 65];
 
-    let result = largest(&number_list);
+    let result = ge::largest(&number_list);
     println!("The largest number is {}", result);
 
     let char_list = vec!['y', 'm', 'a', 'q']; 
 
-    let result = largest(&char_list);
+    let result = ge::largest(&char_list);
     println!("The largest char is {}", result);
 }
